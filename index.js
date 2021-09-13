67570ec25da3892be9df853058d38f4e1690f8d5
OwethuSotomela <owethusotomela@gmail.com>
2021-09-13 23:03:14 +0200

    pizza size

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
 // const bodyParser = require('body-parser');
+var pizzaCart = require("./pizzaCart.js");
 
 const app = express();
 const PORT =  process.env.PORT || 3030;
@@ -19,6 +20,7 @@ app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 let counter = 0;
+var pizzaLog = pizzaCart();
 
 app.get('/', function(req, res) {
 	res.render('index');
@@ -30,24 +32,53 @@ app.post('/count', function(req, res) {
 });
 
 app.post('/orderSmall', function(req, res){
-	
-	res.render('shopping');
+	pizzaLog.OrderSmall(31.99);
+	res.render('index',{
+		Pizza: pizzaLog.GetcartList(),
+	});
 })
 app.post('/orderMedium', function(req, res){
-	res.render('shopping');
+	let medium = req.body.mediumPrice;
+	pizzaLog.OrderMedium(58.99);
+	console.log(pizzaLog.GetcartList())
+	res.render('index',{
+		Pizza: pizzaLog.GetcartList(),
+	});
 })
 app.post('/orderLarge', function(req, res){
-	res.render('shopping');
+	let large = req.body.largePrice;
+	console.log(large);
+	res.render('index',{
+		Pizza: pizzaLog.GetcartList(),
+	});
 })
 app.post('/order', function(req, res){
 	res.render('addPizza')
 })
+
+app.post('/add/:sizeType', function(req, res){
+	const { sizeType } = req.params;
+	console.log(sizeType)
+	pizzaLog.AddQty(sizeType)
+	res.render('index',{
+		Pizza: pizzaLog.GetcartList(),
+	});
+})
+
+app.post('/sub/:sizeType', function(req, res){
+	const { sizeType } = req.params;
+	console.log(sizeType)
+	pizzaLog.SubQty(sizeType)
+	res.render('index',{
+		Pizza: pizzaLog.GetcartList(),
+	});
+})
+
 app.post('/back', function(req, res){
 	res.redirect('/')
 })
 
 // start  the server and start listening for HTTP request on the PORT number specified...
-
 app.listen(PORT, function() {
 	console.log(`App started on port ${PORT}`)
 });
--- pizzaCart.js
+++ pizzaCart.js
@@ -1,27 +1,97 @@
-module.exports = function pizzas(pizza) {
-    console.log(pizza);
+module.exports = () => {
 
-    // amount = "";
-    orderList = []
+    cartList = [];
     pizza = "";
 
     function order() {
-        if (!orderList.includes(pizza)) {
-            orderList.push(pizza);
+        if (!cartList.includes(pizza)) {
+            cartList.push(pizza);
         }
     }
 
-    function countPizza(){
+    function OrderSmall(price) {
+        var samllPizza = cartList.filter(pizza => (pizza.sizeType === "small"));
+        if (samllPizza.length == 0) {
+            cartList.push({
+                sizeType: "small",
+                qty: 1,
+                price: price,
+                origPrice: price
+            });
+        } else {
+            samllPizza[0].qty++;
+            samllPizza[0].price += price;
+        }
+    }
+
+    function AddQty(sizeType) {
+        var pizaSize = cartList.filter(pizza => (pizza.sizeType === sizeType));
+        pizaSize[0].qty++
+        pizaSize[0].price++
+    }
+
+    function SubQty(sizeType) {
+        var pizaSize = cartList.filter(pizza => (pizza.sizeType === sizeType));
+        var pizaSizeRemovedArray = pizaSize[0]
+        if (pizaSizeRemovedArray.qty < 2) {
+            cartList = cartList.filter(pizza => (pizza.sizeType !== sizeType))
+        } else {
+            pizaSizeRemovedArray.qty--
+            pizaSizeRemovedArray.price -= pizaSizeRemovedArray.origPrice
+        }
+    }
+
+    function OrderMedium(price) {
+        var mediumPizza = cartList.filter(pizza => (pizza.sizeType === "medium"));
+        if (mediumPizza.length == 0) {
+            cartList.push({
+                sizeType: "medium",
+                qty: 1,
+                price: price,
+                origPrice: price
+            });
+        } else {
+            mediumPizza[0].qty++;
+            mediumPizza[0].price += price;
+        }
+    }
+
+    function OrderLarge(price) {
+        var largePizza = cartList.filter(pizza => (pizza.sizeType === 'large'));
+        if (largePizza.length == 0) {
+            cartList.push({
+                sizeType: 'large',
+                qty: 1,
+                price: price,
+                origPrice: price
+            })
+        } else {
+            largePizza[0].qty++;
+            largePizza[0].price += price;
+        }
+    }
+
+    function GetcartList() {
+        return cartList;
+    }
+
+    function countPizza() {
         return pizza.length;
     }
 
-    function getTotal(){
-        total = pizza.length
+    function getTotal() {
+        total = pizza.length;
     }
 
     return {
+        OrderSmall,
+        GetcartList,
+        OrderMedium,
         order,
         countPizza,
-        getTotal
-    }
+        getTotal,
+        OrderLarge,
+        AddQty,
+        SubQty
+    };
 }
