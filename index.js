dc40cbde8f51d39d20a861fe9ce0e2431ece4db5
OwethuSotomela <owethusotomela@gmail.com>
2021-09-13 12:43:00 +0200

    new repo

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+// const bodyParser = require('body-parser');
 
 const app = express();
 const PORT =  process.env.PORT || 3017;
 
 // enable the req.body object - to allow us to use HTML forms
+// app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -19,9 +21,7 @@ app.set('view engine', 'handlebars');
 let counter = 0;
 
 app.get('/', function(req, res) {
-	res.render('index', {
-		counter
-	});
+	res.render('pizza');
 });
 
 app.post('/count', function(req, res) {
@@ -31,6 +31,7 @@ app.post('/count', function(req, res) {
 
 
 // start  the server and start listening for HTTP request on the PORT number specified...
+// const PORT = process.env.PORT || 3030
 app.listen(PORT, function() {
 	console.log(`App started on port ${PORT}`)
 });
