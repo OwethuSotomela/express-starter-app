a8de9aed834e6cadae8c941d6756526086360278
OwethuSotomela <owethusotomela@gmail.com>
2021-09-14 17:19:06 +0200

    placing orders

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const exphbs  = require('express-handlebars');
-// const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
 var pizzaCart = require("./pizzaCart.js");
+const { Pool } = require('pg');
 
 const app = express();
-const PORT =  process.env.PORT || 3030;
+const PORT = process.env.PORT || 3030;
 
 // enable the req.body object - to allow us to use HTML forms
-// app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -15,70 +14,92 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 // add more middleware to allow for templating support
-
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-let counter = 0;
-var pizzaLog = pizzaCart();
+let useSSL = false;
+let local = process.env.LOCAL || false;
+if (process.env.DATABASE_URL && !local) {
+	useSSL = true;
+}
 
-app.get('/', function(req, res) {
+const connectionString = process.env.DATABASE_URL || 'postgresql://codex:codex123@localhost:5432/starter_express';
+
+const pool = new Pool({
+	connectionString: connectionString,
+	ssl: {
+		rejectUnauthorized: false
+	}
+});
+
+var pizzaLog = pizzaCart(pool);
+console.log(pool)
+
+app.get('/', function (req, res) {
 	res.render('index');
 });
 
-app.post('/count', function(req, res) {
-	counter++;
-	res.redirect('/')
-});
-
-app.post('/orderSmall', function(req, res){
+app.post('/orderSmall', function (req, res) {
 	pizzaLog.OrderSmall(31.99);
-	res.render('index',{
-		Pizza: pizzaLog.GetcartList(),
+	var Pizza = pizzaLog.GetcartList()
+	res.render('index', {
+		Pizza: Pizza.filter(pizza => (pizza.price).toFixed(2)),
 	});
 })
-app.post('/orderMedium', function(req, res){
-	let medium = req.body.mediumPrice;
+app.post('/orderMedium', function (req, res) {
 	pizzaLog.OrderMedium(58.99);
-	console.log(pizzaLog.GetcartList())
-	res.render('index',{
-		Pizza: pizzaLog.GetcartList(),
+	var Pizza = pizzaLog.GetcartList()
+	res.render('index', {
+		Pizza: Pizza.filter(pizza => (pizza.price).toFixed(2)),
 	});
-})
-app.post('/orderLarge', function(req, res){
-	let large = req.body.largePrice;
-	console.log(large);
-	res.render('index',{
-		Pizza: pizzaLog.GetcartList(),
-	});
-})
-app.post('/order', function(req, res){
-	res.render('addPizza')
+	console.log(Pizza.filter(pizza => (pizza.price).toFixed(2)))
 })
 
-app.post('/add/:sizeType', function(req, res){
+app.post('/orderLarge', function (req, res) {
+	pizzaLog.OrderLarge(98.99);
+	var Pizza = pizzaLog.GetcartList()
+	res.render('index', {
+		Pizza: Pizza.filter(pizza => (pizza.price).toFixed(2)),
+	});
+	console.log(Pizza.filter(pizza => (pizza.price).toFixed(2)))
+})
+app.post('/order', function (req, res) {
+	res.render('orders', {
+		orders: pizzaLog.Orders()
+	})
+})
+
+app.post('/button/:orderId/:statusDecide', function (req, res) {
+	const { orderId, statusDecide } = req.params;
+	pizzaLog.buttonStatus(statusDecide, orderId)
+	res.render('orders', {
+		orders: pizzaLog.Orders()
+	})
+})
+
+app.post('/add/:sizeType', function (req, res) {
 	const { sizeType } = req.params;
 	console.log(sizeType)
 	pizzaLog.AddQty(sizeType)
-	res.render('index',{
+	res.render('index', {
 		Pizza: pizzaLog.GetcartList(),
 	});
 })
 
-app.post('/sub/:sizeType', function(req, res){
+app.post('/sub/:sizeType', function (req, res) {
 	const { sizeType } = req.params;
 	console.log(sizeType)
 	pizzaLog.SubQty(sizeType)
-	res.render('index',{
+	res.render('index', {
 		Pizza: pizzaLog.GetcartList(),
 	});
 })
 
-app.post('/back', function(req, res){
+app.post('/back', function (req, res) {
 	res.redirect('/')
 })
 
 // start  the server and start listening for HTTP request on the PORT number specified...
-app.listen(PORT, function() {
+app.listen(PORT, function () {
 	console.log(`App started on port ${PORT}`)
 });
